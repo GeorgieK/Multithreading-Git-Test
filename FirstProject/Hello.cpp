75520cd2499d275cbbfb11a1184e278d03115b9e
GeorgieK <kalipvp@gmx.net>
2014-06-01 16:44:44 +0200

    test multithreading

--- FirstProject/Hello.cpp
+++ FirstProject/Hello.cpp
@@ -1,15 +1,28 @@
-#include <thread>
 #include <iostream>
+#include <thread>
 
-using std::cout;
-using std::endl;
-using std::thread;
+static const int num_threads = 10;
+
+//This function will be called from a thread
+
+void call_from_thread(int tid) {
+    std::cout << "Launched by thread " << tid << std::endl;
+}
 
 int main() {
-	cout << "Testing my Breakpoints" << endl;
-#ifndef _GLIBCXX_HAS_GTHREADS
-  cout << "GThreads are not supported..." << endl;
-#endif
-  cout << "Hello World!" << endl;
-  return 0;
+    std::thread t[num_threads];
+
+    //Launch a group of threads
+    for (int i = 0; i < num_threads; ++i) {
+        t[i] = std::thread(call_from_thread, i);
+    }
+
+    std::cout << "Launched from the main\n";
+
+    //Join the threads with the main thread
+    for (int i = 0; i < num_threads; ++i) {
+        t[i].join();
+    }
+
+    return 0;
 }
